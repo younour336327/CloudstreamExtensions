1f698d044021638e97a2ec18bf1362d105dd6698
younour336327 <younour336327@gmail.com>
2026-09-06 16:06:35 +0300

    Add root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
